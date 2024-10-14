a432dfc4e8a67d93f514582d8dbefba5bff99bd7
Amos Choo <amoschoouqconnect@gmail.com>
2024-10-14 10:32:56 +1000

    update scripts

--- srabatch.sh
+++ srabatch.sh
@@ -1,14 +1,15 @@
 #!/bin/bash --login
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=24
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=3
+#SBATCH --mem=100G
 #SBATCH --job-name=sra
-#SBATCH --time=200:00:00
+#SBATCH --time=300:00:00
 #SBATCH --partition=general
 #SBATCH --account=a_kelvin_tuong
 #SBATCH -o SRR.out
 #SBATCH -e SRR.error
 
-./names.sh
+# ./names.sh
 ./fastqfetch.sh
+# ./bamfetch.sh
